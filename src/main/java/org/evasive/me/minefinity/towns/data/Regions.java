bdff7895016c4fadbffd8a13b9264f277b5081f6
Ethan-23 <emanroberts@gmail.com>
2026-07-04 18:57:40 -0400

    Added Region Class (Forgot to push with last commit)

---
+++ src/main/java/org/evasive/me/minefinity/towns/data/Regions.java
@@ -0,0 +1,11 @@
+package org.evasive.me.minefinity.towns.data;
+
+public final class Regions {
+    public static final String MAIN_BLOCK = "minefinity_block";
+    public static final String TOWNHALL   = "WORLD_TOWNHALL";
+    public static final String MERCHANT   = "WORLD_MERCHANT";
+    public static final String WORKSHOP   = "WORLD_WORKSHOP";
+    public static final String FORGE      = "WORLD_FORGE";
+
+    private Regions() {}
+}
