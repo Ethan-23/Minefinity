fa5bdd0dd5a1e2d1a0690956c36d1d57309944c2
Ethan-23 <emanroberts@gmail.com>
2026-07-08 18:20:49 -0400

    removed MetalDetector sound just gives stats now

--- src/main/java/org/evasive/me/minefinity/mining/abilities/metaldetector/MetalDetector.java
+++ src/main/java/org/evasive/me/minefinity/mining/abilities/metaldetector/MetalDetector.java
@@ -1,36 +1,15 @@
 package org.evasive.me.minefinity.mining.abilities.metaldetector;
 
-import org.bukkit.entity.Player;
-import org.evasive.me.minefinity.mining.abilities.AbilityNotifier;
 import org.evasive.me.minefinity.mining.abilities.MiningAbility;
-import org.evasive.me.minefinity.mining.abilities.PickaxeAbilities;
-import org.evasive.me.minefinity.mining.context.BreakContext;
 import org.evasive.me.minefinity.mining.context.HitContext;
 
 public class MetalDetector implements MiningAbility {
 
     private static final int METAL_DETECT_CHANCE = 5;
-    private final AbilityNotifier abilityNotifier;
-
-    public MetalDetector(AbilityNotifier abilityNotifier) {
-        this.abilityNotifier = abilityNotifier;
-    }
 
     @Override
     public void applyStats(HitContext context) {
         context.getStatsContext().addSpecialChance(METAL_DETECT_CHANCE);
-    }
-
-    @Override
-    public void onBreak(BreakContext context) {
-        if(!context.statsContext().isSpecialDrop())
-            return;
-
-        sendMetalDetectMessage(context.player());
-    }
-
-    private void sendMetalDetectMessage(Player player){
-        abilityNotifier.abilityNotification(player, PickaxeAbilities.METAL_DETECT, "Proc");
     }
 
 }
